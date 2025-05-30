91b6cbd5c00d8c7933e317e7be8d716c044fc492
TccDeSNUTICAO <tccdesnutri@gmail.com>
2025-05-30 08:14:38 -0300

    Add files via upload

--- RimPorRim/js/Index.js
+++ RimPorRim/js/Index.js
@@ -2,10 +2,6 @@
   function sumirfolder() {
     const footer = document.getElementById('footer');
 
-
-    footer.classList.toggle('hidden');
-
-
     window.scrollTo({
       top: 0,
       behavior: 'smooth' 
